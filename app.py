1aa2dffe1690221d5a2f660f3d51599a662a865f
Mario Perez <marioperezben@hotmail.com>
2022-05-21 20:05:36 -0400

    primer commit
    Primer commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect
+from flask import request
+import requests
+from persistencia import guardar_pedido
+
+app = Flask(__name__)
+
+
+@app.route("/pizza", methods=["POST"])
+def hello_post():
+    nombre = request.form['p1']
+    apellido = request.form['p2']
+    guardar_pedido(nombre,apellido)
+
+    print("Nombre: " + nombre + " Apellido: " + apellido)
+    return redirect("http://localhost:8088/Entrega_final/solicita_pedido.html")
