4de351f6067f511eb1733e1ae1e1641ca32da540
Mario Perez <marioperezben@hotmail.com>
2022-06-03 17:06:10 -0400

    Update test_persistencia.py

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,11 +1,7 @@
-"""
-Pruebas Persistencia
-"""
+""" Pruebas Persistencia """
 import persistencia
 def test_guardar_pedido():
-  """
-  Prueba general
-  """
+  """ Prueba general """
     with open("pedidos.txt", "w+", encoding="utf-8") as file:
         persistencia.guardar_pedido("Pedro", "Gil de Diego")
         persistencia.guardar_pedido("Michael", "Jordan")
